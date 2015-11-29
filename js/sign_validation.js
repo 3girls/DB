3fe4568acd46d3af0488b75d0ca0335153247491
pplntech <pplntech@naver.com>
2015-11-29 23:56:00 +0900

    km_11_29

--- js/sign_validation.js
+++ js/sign_validation.js
@@ -1,9 +1,20 @@
 /*signup validation*/
 function CheckSignupForm() {
+  {/*if there is no emails, set default values*/
+    var input1=document.getElementById("Birth");
+    if(input1.value.length==0) input1.value="2000-01-01";
+    var input2=document.getElementById("Email");
+    if(input2.value.length==0) input2.value="name@domain.com";
+    var input3=document.getElementById("Phone");
+    if(input3.value.length==0) input3.value="01012345678";
+  }
+
   var submit = true;
-  if(!IdCheck())    submit = false;
-  if(!PwCheck())    submit = false;
+  if(!IdCheck()) submit = false;
+  if(!PwCheck()) submit = false;
   if(!PwreCheck())  submit = false;
+  if(!NameCheck()) submit = false;
+
   return submit;
 }
 
@@ -22,24 +33,21 @@ function IdCheck() {
   return check;
 }
 
-/*pw check*/
 function PwCheck() {
   var lengthcheck = true;
 
   var pw = document.getElementById("PW").value;
 
-  if(pw.length<6)
+  if(pw==null || pw=="" || pw.length<6){
+    document.getElementById("pw_msg").innerHTML = "비밀번호는 6자 이상이어야 합니다";
     lengthcheck = false;
-
-  if(!lengthcheck) {
-    document.getElementById("pwc_msg").innerHTML = "비밀번호는 6자 이상이어야 합니다";
-    return false;
   }
-  else
-    return true;
+
+  return lengthcheck;
+
 }
 
-/*pwre check*/
+
 function PwreCheck() {
   var check = true;
 
@@ -48,6 +56,7 @@ function PwreCheck() {
 
   if(pw.length!=pwre.length)
     check = false;
+
   else {
     for(var i=0; i<pw.length; i++)
       if(pw.charAt(i)!=pwre.charAt(i))
@@ -58,4 +67,18 @@ function PwreCheck() {
     document.getElementById("pwrecheck_msg").innerHTML = "비밀번호가 일치하지 않습니다";
 
   return check;
+}
+
+function NameCheck() {
+  var check = true;
+
+  var name = document.getElementById("Name").value;
+
+  if(name==null || name==""){
+    document.getElementById("name_msg").innerHTML = "이름을 입력하세요.";
+    check = false;
+  }
+
+  return check;
+
 }
--- signup.php
+++ signup.php
@@ -6,7 +6,7 @@
   $_SESSION['login'] = 'NO';
   $_SESSION['id'] = 'NO';
 
-  $myhost = 'mysql.hostinger.kr';
+  $myhost = '31.170.164.40';
   $myid = 'u729743068_37';
   $mypw = '123456';
   $con = mysql_connect($myhost, $myid, $mypw);
@@ -24,35 +24,54 @@
   $email = $_POST['Email'];
   $phone = $_POST['Phone'];
 
+
   $query = "select count(*) from Administrator where ID='$id'";
   $result = mysql_query($query,$con);
+  if(!$result){
+    $message  = 'Invalid query: ' . mysql_error() . "\n";
+    $message .= 'Whole query: ' . $query;
+    die($message);
+  }
   $r1 = mysql_fetch_row($result);
 
-  $query = "select count(*) from Administrator where ID='$id'";
+  $query = "select count(*) from Submitter where ID='$id'";
   $result = mysql_query($query,$con);
+  if(!$result){
+    $message  = 'Invalid query: ' . mysql_error() . "\n";
+    $message .= 'Whole query: ' . $query;
+    die($message);
+  }
   $r2 = mysql_fetch_row($result);
 
-  $query = "select count(*) from Administrator where ID='$id'";
+  $query = "select count(*) from Evaluator where ID='$id'";
   $result = mysql_query($query,$con);
+  if(!$result){
+    $message  = 'Invalid query: ' . mysql_error() . "\n";
+    $message .= 'Whole query: ' . $query;
+    die($message);
+  }
   $r3 = mysql_fetch_row($result);
-  echo $r1[0].$r2[0].$r3[0];
   $reg_num = $r1[0] + $r2[0] + $r3[0];
+  //echo "$id $pw $pw_check $usertype $name $gender $birth $email $phone";
 
   if($reg_num>0){
-  echo "<script>
-  alert('[중복된 아이디]\r\n\r\n 다른 아이디를 선택하세요.');
-  history.back();
-  </script>";
-  die;
+  echo "<script>alert('중복된아이디입니다.');history.back();</script>";
   }
-
-  $query = "insert into '$usertype' (ID, PW, Name, Gender, Email, Birth, Phone) ";
+  else{
+  $query = "insert into $usertype (ID, PW, Name, Gender, Email, Birth, Phone) ";
   $query.= "values ('$id','$pw','$name','$gender','$email','$birth','$phone')";
   $result = mysql_query($query, $con);
-
+  if(!$result){
+    $message  = 'Invalid query: ' . mysql_error() . "\n";
+    $message .= 'Whole query: ' . $query;
+    die($message);
+  }
+  else{
   echo "<script>alert('회원가입 성공!');
   location.replace('login.php');
   </script>";
+  }
+  }
 ?>
 </body>
 </html>
