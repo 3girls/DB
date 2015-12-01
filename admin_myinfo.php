b561d9fe5f6941954b250e428b12cf15bf102c7e
limminyong <minny706@naver.com>
2015-12-01 11:30:02 +0900

    test

--- admin_myinfo.php
+++ admin_myinfo.php
@@ -8,7 +8,7 @@ session_start();
   $myhost = 'mysql.hostinger.kr';
   $myid = 'u729743068_37';
   $mypw = '123456';
-  $con = mysql_connect($myhost, $myid, $mypw);
+  $con = @mysql_connect($myhost, $myid, $mypw);
 
   $db = mysql_select_db("u729743068_37");
   mysql_query("SET NAMES utf8"); //한글처리
