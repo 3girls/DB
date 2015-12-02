8217b48d3a9526c6a4c08e36a77325ded6c230fa
pplntech <pplntech@naver.com>
2015-12-02 14:11:30 +0900

    changed basic.php

--- basic.php
+++ basic.php
@@ -4,9 +4,9 @@
   $myhost = 'mysql.hostinger.kr';
   $myid = 'u729743068_37';
   $mypw = '123456';
-  $con = mysql_connect($myhost, $myid, $mypw);
+  $con = @mysql_connect($myhost, $myid, $mypw);
 
-  $db = mysql_select_db("u729743068_37");
-  mysql_query("SET NAMES utf8"); //한글처리
+  $db = @mysql_select_db("u729743068_37");
+  @mysql_query("SET NAMES utf8"); //한글처리
   
 ?>
