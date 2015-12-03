7f7efa3e5a7cc11cd32fd49ac70d9f6fbecd2f92
HannaKimCS <cs.hannakim@gmail.com>
2015-12-03 13:18:53 +0900

    file upload

--- submitter_taskup_do.php
+++ submitter_taskup_do.php
@@ -15,7 +15,7 @@
   $file_type = substr(strrchr($file_name,"."),1);
 
   # 파일 확장자가 .csv가 아니면 제출 거절
-  if($file_type != 'csv') {
+  if($file_type != '.csv') {
     echo "<script>alert('csv파일만 업로드하실 수 있습니다.'); history.back();</script>";
   }
 
