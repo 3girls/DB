698e1dc456c586798260070d00538a7a7cb3e6f6
HannaKimCS <cs.hannakim@gmail.com>
2015-12-04 14:12:07 +0900

    syntax error fix

--- evaluator_grade.php
+++ evaluator_grade.php
@@ -12,7 +12,7 @@ $eid = $_SESSION['id'];
   $grade = $_POST['grade'];
   $fid = $_GET['fid'];
 
-  $query = "UPDATE Parsing_Sequence_Data_Type SET EvaulatorGrade='".$grade."', Estate='1' WHERE ID='".$fid."'";
+  $query = "UPDATE Parsing_Sequence_Data_Type SET EvaulatorGrade=".$grade.", Estate=1 WHERE ID='".$fid."'";
   mysql_query($query, $con);
 
   #총 평가점수를 계산하여 패스 여부를 결정
@@ -23,12 +23,12 @@ $eid = $_SESSION['id'];
   $X = ($arr['DuplicateTupleNum']/$arr['TotalTupleNum'] + $arr['NullRatio']*0.01)*50 + $arr['EvaluatorGrade'];
   #PASS
   if($X >= 100) {
-    $query = "UPDATE Parsing_Sequence_Data_Type SET P_NP='1' WHERE ID='".$fid."'";
+    $query = "UPDATE Parsing_Sequence_Data_Type SET P_NP=1 WHERE ID='".$fid."'";
     mysql_query($query, $con);
   }
   #NONPASS
   else {
-    $query = "UPDATE Parsing_Sequence_Data_Type SET P_NP='0' WHERE ID='".$fid."'";
+    $query = "UPDATE Parsing_Sequence_Data_Type SET P_NP=0 WHERE ID='".$fid."'";
     mysql_query($query, $con);
   }
 
