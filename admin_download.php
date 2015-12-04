bbaef63c35875e592ad29b41c357b16266a645d2
pplntech <pplntech@naver.com>
2015-12-04 20:03:40 +0900

    download

--- admin_download.php
+++ admin_download.php
@@ -24,16 +24,46 @@ session_start();
   $tempname .= $taskname;
   $tempname .= ".csv";
 
-	$query = "SELECT * INTO OUTFILE '$tempname'
-	FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"'
-	LINES TERMINATED BY \"\n\"
-	FROM $taskTableName";
-  $TS_result = mysql_query($query, $con);
-  if(!$TS_result){
-    $message  = 'Invalid query: ' . mysql_error() . "\n";
-    $message .= 'Whole query: ' . $query;
-    die($message);
-  }
+  //header to give the order to the browser
+	header('Content-Type: text/csv');
+	header('Content-Disposition: attachment;filename=$tempname');
+
+	//select table to export the data
+	$select_table=mysql_query('select * from $taskTableName');
+	$rows = mysql_fetch_assoc($select_table);
+
+	if ($rows)
+	{
+		getcsv(array_keys($rows));
+	}
+	while($rows)
+	{
+		getcsv($rows);
+		$rows = mysql_fetch_assoc($select_table);
+	}
+
+	// get total number of fields present in the database
+	function getcsv($no_of_field_names)
+	{
+		$separate = '';
+
+
+		// do the action for all field names as field name
+		foreach ($no_of_field_names as $field_name)
+		{
+			if (preg_match('/\\r|\\n|,|"/', $field_name))
+			{
+				$field_name = '' . str_replace('', $field_name) . '';
+			}
+			echo $separate . $field_name;
+
+			//sepearte with the comma
+			$separate = ',';
+		}
+
+		//make new row and line
+		echo "\r\n";
+	}
 
   ?>
 </body>
