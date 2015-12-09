6c37735c3e55e7bd93f198503e16e4adb2c3187a
HannaKimCS <cs.hannakim@gmail.com>
2015-12-09 21:23:18 +0900

    index page fix

--- index.php
+++ index.php
@@ -4,25 +4,23 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<meta http-equiv="refresh" content="0;url=login.php">
-<title>DataCollector</title>
-<script language="javascript">
-    window.location.href = "login.php"
-</script>
-</head>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>
 <body>
   <?php
-  if($_SESSSION['login'] == 'YES'){
-    if($_SESSION['usertype'] == 1){ //administrator
+  if($_SESSSION['login'] == 'YES') {
+    if($_SESSION['usertype'] == 1) { //administrator
       echo "<script>location.replace('admin_search.php');</script>";
     }
-    else if($_SESSION['usertype'] == 2){
+    else if($_SESSION['usertype'] == 2) {
       echo "<script>location.replace('submitter_task.php');</script>";
     }
-    else if($_SESSION['usertype'] == 3){
+    else if($_SESSION['usertype'] == 3) {
       echo "<script>location.replace('evaluator_waiting.php');</script>";
     }
+  }
+  else {
+    echo "<script>location.replace('login.php');</script>";
   }
   ?>
 Go to <a href="login.php">login page</a>
